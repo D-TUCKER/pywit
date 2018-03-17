192244298bb70a63ecf997f423bfc70fb509456e
D-TUCKER <darren.s.tucker@gmail.com>
2018-03-17 14:31:58 -0400

    add some more requirements

--- setup.py
+++ setup.py
@@ -12,22 +12,22 @@ except ImportError:
     from distutils.command.build_py import build_py
 
 # install_requires = ['prompt_toolkit']
-if sys.version_info < (2, 6):
-    warnings.warn(
-        'Python 2.5 is no longer officially supported by Wit. '
-        'If you have any questions, please file an issue on Github or '
-        'contact us at help@wit.ai.',
-        DeprecationWarning)
-    install_requires.append('requests >= 0.8.8, < 0.10.1')
-    install_requires.append('ssl')
-else:
-    install_requires.append('requests >= 0.8.8')
+# if sys.version_info < (2, 6):
+#     warnings.warn(
+#         'Python 2.5 is no longer officially supported by Wit. '
+#         'If you have any questions, please file an issue on Github or '
+#         'contact us at help@wit.ai.',
+#         DeprecationWarning)
+#     install_requires.append('requests >= 0.8.8, < 0.10.1')
+#     install_requires.append('ssl')
+# else:
+#     install_requires.append('requests >= 0.8.8')
 
-if sys.version_info < (3, 0):
-    try:
-        from util import json
-    except ImportError:
-        install_requires.append('simplejson')
+# if sys.version_info < (3, 0):
+#     try:
+#         from util import json
+#     except ImportError:
+#         install_requires.append('simplejson')
 
 setup(
     name='wit',
